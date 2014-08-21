70bf912782b45b8e65aeb174adc58d3bb2a6a07d
Adam C. Baker <adam.durandal@gmail.com>
2014-08-20 17:30:27 -0700

    add export options for the browser.

--- form_error.js
+++ form_error.js
@@ -1,39 +1,49 @@
-function FormError(field, message){
-  if(this instanceof FormError){
-    if(field && message) {
-      this[field] = [message];
-    }
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define([], factory);
+  } else if (typeof exports === 'object') {
+    module.exports = factory();
   } else {
-    return new FormError(field, message);
+    root.FormError = factory();
   }
-};
-
-FormError.prototype.concat = function(other){
-  var aggregated = new FormError();
-  for(var key in this) {
-    if(this.hasOwnProperty(key)) {
-      aggregated[key] = this[key];
+}(this, function () {
+  function FormError(field, message){
+    if(this instanceof FormError){
+      if(field && message) {
+        this[field] = [message];
+      }
+    } else {
+      return new FormError(field, message);
     }
-  }
-  for(var key in other) {
-    if(other.hasOwnProperty(key)) {
-      var val = aggregated[key], errs = other[key];
-      aggregated[key] = val ? val.concat(errs) : errs;
-    }
-  }
-  return aggregated;
-};
+  };
 
-FormError.id = FormError.prototype.id = new FormError();
-FormError.empty = FormError.prototype.empty = FormError;
-FormError.fromObject = function(obj){
-  var err = new FormError;
-  for(var key in obj){
-    if(obj.hasOwnProperty(key)) {
-      err[key] = obj[key];
+  FormError.prototype.concat = function(other){
+    var aggregated = new FormError();
+    for(var key in this) {
+      if(this.hasOwnProperty(key)) {
+        aggregated[key] = this[key];
+      }
     }
-  }
-  return err;
-}
+    for(var key in other) {
+      if(other.hasOwnProperty(key)) {
+        var val = aggregated[key], errs = other[key];
+        aggregated[key] = val ? val.concat(errs) : errs;
+      }
+    }
+    return aggregated;
+  };
 
-module.exports = FormError;
+  FormError.id = FormError.prototype.id = new FormError();
+  FormError.empty = FormError.prototype.empty = FormError;
+  FormError.fromObject = function(obj){
+    var err = new FormError;
+    for(var key in obj){
+      if(obj.hasOwnProperty(key)) {
+        err[key] = obj[key];
+      }
+    }
+    return err;
+  }
+
+  return FormError;
+}));
